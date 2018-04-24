98bd465c8a7b73589e224ea947f22d14ae23c9e2
keith <keith@raincityambience.com>
2018-04-23 19:51:39 -0700

    File restructure. Basic routing.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import fbase from './config';
 
-import Chatroom from './Chatroom';
+import Chatroom from './components/chatroom/Chatroom';
+import Login from './components/login/Login';
 
 class App extends Component {
   // componentDidMount(){
@@ -15,19 +17,47 @@ class App extends Component {
   //   })
   // }
 
+  constructor(){
+    super();
 
-  render() {    
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Chatroom />
-
-      </div>
-    );
+    this.state = {
+      user: true
+    }
   }
+
+
+render(){
+  return (
+
+    <BrowserRouter>
+
+        <Switch>
+          <Route exact path="/" component={() => !this.state.user ? <Login /> : <Redirect to="/dashboard"/>}/>
+
+          <Route exact path="/dashboard" component={Chatroom}/>
+
+          <Route component={() => !this.state.user ? <Login /> : <Redirect to="/dashboard"/>}/>
+
+        </Switch>
+
+    </BrowserRouter>
+
+  )
+
+}
+
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <h1 className="App-title">Welcome to React</h1>
+  //       </header>
+  //       <Chatroom />
+  //
+  //     </div>
+  //   );
+  // }
 }
 
 export default App;
--- src/components/chatroom/Chatroom.js
+++ src/components/chatroom/Chatroom.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Message from './Message'
+import Message from '../message/Message'
 import firebase from 'firebase';
 import './chatroom.css';
 
