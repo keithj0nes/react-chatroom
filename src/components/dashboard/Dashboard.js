b27289e7417bb6305f9f78f6ae12f840829bfd64
keith <keith@raincityambience.com>
2018-04-24 19:33:19 -0700

    Create Dashboard component for logged in user redirect

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+
+
+  render(){
+    return (
+      <h2>Hello from Dashboard</h2>
+    )
+  }
+}
+
+export default Dashboard;
